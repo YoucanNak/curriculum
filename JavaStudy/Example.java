79df0a906e7f3e30131e9324f06b9d239318698b
YoucanNak <106541437+YoucanNak@users.noreply.github.com>
2022-07-20 08:02:20 +0900

    ３章チェックテスト
    テスト課題Main.javaの作成

---
+++ JavaStudy/Example.java
@@ -0,0 +1,14 @@
+public class Example {
+
+   public static void main(String[] args) {
+      int hogeint = 50;
+      String hogeString = "テスト";
+      //double型の hogeDouble という名前の変数を宣言し、値「0.2」で初期化
+      double hogeDouble = 0.2;
+      hogeint = 100;
+      hogeString = "合格";
+      System.out.println(hogeint);
+      System.out.println(hogeString);
+      System.out.println(hogeDouble);
+   } 
+}
